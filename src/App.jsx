431ab0566803fc568f116eae3120eea56005e272
dh <dohyun134@naver.com>
2025-01-10 15:08:05 +0900

    프로젝트관리

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Hero from "./components/Hero";
+import Projects from "./components/Projects";
+import "./App.css";
+
+const App = () => {
+  return (
+    <>
+      <Hero />
+      <Projects />
+    </>
+  );
+};
+
+export default App;
---
+++ src/data.js
@@ -0,0 +1,31 @@
+import p1 from "./assets/memory3.png";
+import p2 from "./assets/movieapp.png";
+import p3 from "./assets/imagesearch.png";
+import p4 from "./assets/finder.png";
+
+export const projects = [
+  {
+    id: 1,
+    title: "Memory Game",
+    url: "https://suckgugame.netlify.app/",
+    image: p1,
+  },
+  {
+    id: 2,
+    title: "Movie search",
+    url: "https://lunamovie-app.netlify.app/",
+    image: p2,
+  },
+  {
+    id: 3,
+    title: "Image search",
+    url: "https://imagesearch-luna.netlify.app/",
+    image: p3,
+  },
+  {
+    id: 4,
+    title: "Github Finder",
+    url: "https://finder-luna.netlify.app/",
+    image: p4,
+  },
+];
