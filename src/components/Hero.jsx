f5176302a5bbcc86f2cf34957f5451104080f33e
dh <dohyun134@naver.com>
2025-01-10 15:13:01 +0900

    글수정

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -7,13 +7,7 @@ function Hero() {
       <div className="hero-center">
         <div className="hero-title">
           <h1>React Projects</h1>
-          <p>
-            Single-origin coffee deep v sus small batch. Gorpcore vape
-            lumbersexual normcore four dollar toast drinking vinegar. Twee 90's
-            taiyaki small batch bitters, bespoke jianbing leggings marxism
-            keytar pabst adaptogen mixtape tumblr stumptown. DSA twee iceland
-            tacos swag tote bag. Solarpunk man bun fingerstache retro pinterest.
-          </p>
+          <p>루나가 리액트로 만든 프로젝트들</p>
         </div>
         <div className="img-container">
           <img src={heroImg} alt="woman and the browser" className="img" />
